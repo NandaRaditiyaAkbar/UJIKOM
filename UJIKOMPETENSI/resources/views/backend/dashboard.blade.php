7b47bb764569d774020e7cdf31c5634fe2f1036f
RandyR025 <71814522+RandyR025@users.noreply.github.com>
2023-05-20 00:25:08 +0700

    template admin

---
+++ UJIKOMPETENSI/resources/views/backend/dashboard.blade.php
@@ -0,0 +1,3 @@
+@extends('backend/layouts.template')
+@section('content')
+@endsection
---
+++ UJIKOMPETENSI/resources/views/backend/layouts/template.blade.php
@@ -0,0 +1,12 @@
+@include('backend/layouts.header')
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
+</div>
+<!-- /.container-fluid -->
+</div>
+@yield('content')
+
+@include('backend/layouts.footer')
